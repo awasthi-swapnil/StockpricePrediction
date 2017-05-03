b2193b5cfd58876649f411c98917cddad913a45c
swapnilawasthi <swapniliec@gmail.com>
2017-05-03 16:22:01 -0400

    +1

--- sdmprojectRcode.R
+++ sdmprojectRcode.R
@@ -1,13 +1,24 @@
 setwd("F:/Study/SDM")
-jetfuel <- read.csv(file = 'jetfuelprices.csv')
-delta <- read.csv(file = 'Deltastocks.csv')
-sp500 <- read.csv(file = 'Proj-SP500.csv')
-brentoil <- read.csv(file = 'brentoilprices.csv')
+jetfuel <- read.csv(file = 'jetfuelprices.csv', skip = 12, header = T, as.is = T, na.strings = "NA")
+delta <- read.csv(file = 'Deltastocks.csv', skip = 0, header = T, as.is = T, na.strings = "NA")
+sp500 <- read.csv(file = 'Proj-SP500.csv', skip = 0, header = T, as.is = T, na.strings = "NA")
+brentoil <- read.csv(file = 'brentoilprices.csv', skip = 12, header = T, as.is = T, na.strings = "NA")
 
-#sp500$adjclose <- sp500[,]
-head(sp500,15)
-head(delta)
-tail(delta)
+colSums(is.na(jetfuel))
+colSums(is.na(delta))
+colSums(is.na(sp500))
+colSums(is.na(brentoil))
+
+#setting up the dates format and extracting required columns
+sp500$Date <- as.Date(sp500$Date, '%Y-%m-%d')
+sp500.data <- sp500[,c('Date','Adj.Close')]
+
+delta$Date <- as.Date(delta$Date, '%Y-%m-%d')
+delta.data <- delta[,c('Date','Adj.Close')]
+
+jetfuel$DATE <- as.Date(jetfuel$DATE, '%Y-%m-%d')
+brentoil$DATE <- as.Date(brentoil$DATE, '%Y-%m-%d')
+
 
 fuel <- fuel[13:nrow(fuel),]
 #changing name of column 
