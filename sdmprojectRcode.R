9b0820c447bd21f9d11df1b9b9e43bb6d750ea41
swapnilawasthi <swapniliec@gmail.com>
2017-05-03 16:41:33 -0400

    a3

--- sdmprojectRcode.R
+++ sdmprojectRcode.R
@@ -1,9 +1,12 @@
 setwd("F:/Study/SDM")
+
+#reading file and setting the junks values to NA
 jetfuel <- read.csv(file = 'jetfuelprices.csv', skip = 12, header = T, as.is = T, na.strings = "#N/A")
 delta <- read.csv(file = 'Deltastocks.csv', skip = 0, header = T, as.is = T, na.strings = "NA")
 sp500 <- read.csv(file = 'Proj-SP500.csv', skip = 0, header = T, as.is = T, na.strings = "NA")
 brentoil <- read.csv(file = 'brentoilprices.csv', skip = 12, header = T, as.is = T, na.strings = "#N/A")
 
+#checking the NA values
 colSums(is.na(jetfuel))
 colSums(is.na(delta))
 colSums(is.na(sp500))
@@ -18,5 +21,34 @@ delta.data <- delta[,c('Date','Adj.Close')]
 
 jetfuel$DATE <- as.Date(jetfuel$DATE, '%Y-%m-%d')
 brentoil$DATE <- as.Date(brentoil$DATE, '%Y-%m-%d')
+
+#converting the NAs to mean values of the column - Imputation
+jetfuel[is.na(jetfuel[,2]), 2] <- mean(jetfuel[,2], na.rm = TRUE)
+brentoil[is.na(jetfuel[,2]), 2] <- mean(brentoil[,2], na.rm = TRUE)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
